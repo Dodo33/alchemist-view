71d139d8607e1a3d86179dedfdc5428b2714cb8d
Dodo33 <dodo.33@gmx.com>
2018-04-28 14:47:37 +0200

    Remove useless tag

--- alchemist-view/left_panel.php
+++ alchemist-view/left_panel.php
@@ -4,21 +4,19 @@
 				<img src="img/logo.png"/>
 			</div>
 			<div class="menu_left_panel">
-				<form action="ts.php" method="POST">
-					<ul>
-						<li><a href="dashboard.php">Dashboard</a></li>
+				<ul>
+					<li><a href="dashboard.php">Dashboard</a></li>
 
-						<?php
-							$sql = "SELECT ts_name FROM ts";
-							$res = $mysqli->query($sql);
+					<?php
+						$sql = "SELECT ts_name FROM ts";
+						$res = $mysqli->query($sql);
 
-							while ($row = $res->fetch_assoc()) 
-								echo "<li><a href='ts.php?ts_name=" . $row["ts_name"] . "'>" . $row["ts_name"] . "</a></li>";
-							
-						?>
+						while ($row = $res->fetch_assoc()) 
+							echo "<li><a href='ts.php?ts_name=" . $row["ts_name"] . "'>" . $row["ts_name"] . "</a></li>";
+						
+					?>
 
-					</ul>
-				</form>
+				</ul>
 			</div>
 		</div>
 
